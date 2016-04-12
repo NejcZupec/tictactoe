585a487fd735ce6d7aea33ae907a6774799bdf80
nzupec <nzupec@nil.si>
2016-04-12 09:23:00 +0200

    Implement is_game_ended. Add more unit tests.

--- game/game.py
+++ game/game.py
@@ -2,12 +2,13 @@ class Game:
     """
     A tic-tac-toe game.
     """
-    board = []
 
     def __init__(self):
         self.board = [[' ' for _ in range(3)] for _ in range(3)]
+        self.moves = []  # every move is presented as a tuple (player, x, y)
 
     def print_board(self):
+        print
         for i, row in enumerate(self.board):
             print '%s | %s | %s' % tuple(row)
             print '---------' if not i == 2 else ''
@@ -16,8 +17,40 @@ class Game:
         assert player in ['o', 'x'], "A possible player is only 'o' or 'x'."
         assert x in [0, 1, 2], "X should be 0, 1 or 2."
         assert y in [0, 1, 2], "Y should be 0, 1 or 2."
+        assert self.board[x][y] not in ['o', 'x'], "This position is already occupied."
+        assert len(self.moves) == 0 or not self.moves[-1][0] == player, "The other player should play."
 
         self.board[x][y] = player
+        move = (player, x, y)
+        self.moves.append(move)
+
+    def is_game_ended(self):
+        if len(self.moves) > 4:
+
+            # diagonal win
+            d1 = [self.board[i][i] for i in range(len(self.board))]
+            if self.all_equal(d1):
+                return True
+
+            d2 = [row[i] for i, row in enumerate(self.board)]
+            if self.all_equal(d2):
+                return True
+
+            # horizontal win
+            for row in self.board:
+                if self.all_equal(row):
+                    return True
+
+            # vertical win
+            for row in zip(*self.board):  # transpose board
+                if self.all_equal(row):
+                    return True
+
+        return False
+
+    @staticmethod
+    def all_equal(row):
+        return not row[0] == ' ' and row[1:] == row[:-1]
 
 
 if __name__ == '__main__':
@@ -28,5 +61,7 @@ if __name__ == '__main__':
     g.move('x', 0, 1)
     g.print_board()
     g.move('o', 0, 0)
+    g.print_board()
+    g.move('x', 1, 1)
     g.print_board()
 
--- game/tests.py
+++ game/tests.py
@@ -42,6 +42,20 @@ class GameTest(TestCase):
         with self.assertRaises(AssertionError):
             g.move('o', 0, 5)
 
+    def test_already_played_move_cant_be_played_again(self):
+        g = Game()
+
+        g.move('o', 1, 1)
+        with self.assertRaises(AssertionError):
+            g.move('x', 1, 1)
+
+    def test_consecutive_moves_not_the_same_player(self):
+        g = Game()
+
+        g.move('x', 0, 0)
+        with self.assertRaises(AssertionError):
+            g.move('x', 1, 0)
+
     def test_illegal_players(self):
         g = Game()
 
@@ -53,5 +67,61 @@ class GameTest(TestCase):
 
         with self.assertRaises(AssertionError):
             g.move('-', 0, 0)
+
+    def test_moves(self):
+        g = Game()
+        m1 = ('o', 1, 1)
+        m2 = ('x', 2, 2)
+        m3 = ('o', 0, 1)
+
+        g.move(*m1)
+        g.move(*m2)
+        g.move(*m3)
+        self.assertListEqual([m1, m2, m3], g.moves)
+
+    def test_is_game_ended(self):
+        # game 1 - diagonal win
+        g = Game()
+        g.move('x', 0, 0)
+        g.move('o', 0, 1)
+        g.move('x', 1, 1)
+        g.move('o', 1, 2)
+        self.assertFalse(g.is_game_ended(), "Game is not ended yet.")
+        g.move('x', 2, 2)
+        self.assertTrue(g.is_game_ended(), "Game should ended - diagonal win.")
+
+        # game 2 - horizontal win
+        g = Game()
+        g.move('x', 0, 0)
+        g.move('o', 1, 1)
+        g.move('x', 0, 1)
+        g.move('o', 1, 2)
+        self.assertFalse(g.is_game_ended(), "Game is not ended yet.")
+        g.move('x', 0, 2)
+        self.assertTrue(g.is_game_ended(), "Game should ended - horizontal win.")
+
+        # game 3 - vertical win
+        g = Game()
+        g.move('x', 0, 0)
+        g.move('o', 0, 1)
+        g.move('x', 1, 0)
+        g.move('o', 1, 1)
+        self.assertFalse(g.is_game_ended(), "Game is not ended yet.")
+        g.move('x', 2, 0)
+        self.assertTrue(g.is_game_ended(), "Game should ended - vertical win.")
+
+        # game 4 - more than 5 moves
+        g = Game()
+        g.move('x', 0, 0)
+        g.move('o', 0, 1)
+        g.move('x', 1, 1)
+        g.move('o', 1, 2)
+        g.move('x', 2, 1)
+        g.move('o', 0, 2)
+        self.assertFalse(g.is_game_ended(), "Game is not ended yet.")
+        g.move('x', 2, 2)
+        self.assertTrue(g.is_game_ended(), "Game should ended - vertical win.")
+
+
 
 
