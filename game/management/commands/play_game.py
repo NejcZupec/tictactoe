d344af5df6eb53b43e0c6fa8105648df8bff2a7e
nzupec <zupec.nejc@gmail.com>
2016-04-12 13:22:23 +0200

    Improve CLI play game. Add option to select first player.

--- game/management/commands/play_game.py
+++ game/management/commands/play_game.py
@@ -5,34 +5,54 @@ from game.game import Game
 
 class Command(BaseCommand):
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--first_player',
+            default='x',
+            help="Set which player should start ('x' or 'o')",
+        )
+
     def handle(self, *args, **options):
+        player = options['first_player']
 
         # start a new game
         g = Game()
         g.print_board()
 
         while True:
-            player = 'x' if len(g.moves) % 2 == 0 else 'o'
-            print "Player %c:" % player
+            print "Player %c" % player
 
-            x = int(raw_input("Enter x-coordinate: "))
-            y = int(raw_input("Enter y-coordinate: "))
+            x = self.get_input("Enter x-coordinate: ")
+            if x is None:
+                continue
+
+            y = self.get_input("Enter y-coordinate: ")
+            if y is None:
+                continue
 
             try:
                 g.move(player, x, y)
+                g.print_board()
+
+                end_game_status = g.get_winner_or_draw()
+
+                if end_game_status:
+                    if end_game_status == 'draw':
+                        print "Draw."
+                    else:
+                        print "Player %c wins." % end_game_status
+                    break
+
+                # change player
+                player = 'o' if player == 'x' else 'x'
+
             except AssertionError, msg:
                 print "-----------\n%s\n-----------" % msg
 
-            g.print_board()
-
-            end_game_status = g.get_winner_or_draw()
-
-            if end_game_status:
-                if end_game_status == 'draw':
-                    print "Draw."
-                else:
-                    print "Player %c wins." % end_game_status
-                break
-
-
-
+    @staticmethod
+    def get_input(msg):
+        try:
+            return int(raw_input(msg))
+        except ValueError:
+            print "-----------\n%s\n-----------" % "Only integer is accepted."
+            return
---
+++ game/test/test_command_play_game.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+
+
+class CommandGamePlayTest(TestCase):
+
+    def test_first_player_argument(self):
+        pass
+
+    def test_game(self):
+        pass
+
+    def test_illegal_coordinates(self):
+        pass
