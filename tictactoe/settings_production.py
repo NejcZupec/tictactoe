4b0784c208185d0f5f9f4126d0c597038d34ec6c
CircleCI <circleci@reciprocitylabs.com>
2020-03-28 20:43:41 +0100

    Set up whitenoise to serve static files

--- tictactoe/settings_production.py
+++ tictactoe/settings_production.py
@@ -13,20 +13,29 @@ def get_env_variable(var_name):
         error_msg = 'Set the %s environment variable' % var_name
         raise ImproperlyConfigured(error_msg)
 
+
 DEBUG = False
 
-ALLOWED_HOSTS = ['tictactoe.zupec.net']
+# TODO: temporarily disabled to test Heroku
+# ALLOWED_HOSTS = ['tictactoe.zupec.net']
 
 SECRET_KEY = get_env_variable('SECRET_KEY')
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': get_env_variable("DATABASE_NAME"),
-        'USER': get_env_variable("DATABASE_USER"),
-        'PASSWORD': get_env_variable("DATABASE_PASSWORD"),
-        'HOST': get_env_variable("DATABASE_HOST"),
-        'PORT': '5432',
-    },
-}
+MIDDLEWARE_CLASSES += (
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+)
+
+# TODO: temporarily disabled to test Heroku
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': get_env_variable("DATABASE_NAME"),
+#         'USER': get_env_variable("DATABASE_USER"),
+#         'PASSWORD': get_env_variable("DATABASE_PASSWORD"),
+#         'HOST': get_env_variable("DATABASE_HOST"),
+#         'PORT': '5432',
+#     },
+# }
 
